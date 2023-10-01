71786530c062f0f80446e109bb12e52674cdc8ab
devereaweaver <devereweaver_dev@icloud.com>
2023-09-30 22:01:34 -0400

    updated comments

--- Assignments/Programming-Assignment-1/Problem1.py
+++ Assignments/Programming-Assignment-1/Problem1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """
 Name: Devere Anthony Weaver
 
@@ -9,60 +10,63 @@ Description: TODO
 """
 import random
 
+
 def BinarySearch(array, key):
-  """
-  
-  Implementation of the binary search algorithm. The function
-  takes an array-like structure and searches for the given key. This
-  implementation of binary search returns the index of the search key,
-  the value of the key once found, and the total number of guess until
-  the key was found.
+    """
 
-  Input:
-    1. array - an array values to be searched
-    2. key - the particular value to be searched for in array
+    Implementation of the binary search algorithm. The function
+    takes an array-like structure and searches for the given key. This
+    implementation of binary search returns the index of the search key,
+    the value of the key once found, and the total number of guess until
+    the key was found.
 
-  Return:
-    1. packed tuple containing:
-      * the key's index if found or -1 if the key is not found 
-      * value - the value of the key itself
-      * the number of iterations (guesses) until the key was found
-  
-  """
-  low = 1
-  high = len(array) - 1
-  num_guesses = 0
+    Input:
+      1. array - an array values to be searched
+      2. key - the particular value to be searched for in array
 
-  while(low <= high):
-    num_guesses += 1
-    middle = (low + high) // 2
-    if (array[middle] < key):    # shift left
-      print(f"The number is higher than {middle}.")
-      low = middle + 1
-    elif (array[middle] > key):    # shift right
-      print(f"The number is lower than {middle}.")
-      high = middle - 1
-    elif (array[middle] == key):
-      print(f"\nThe number is found. It is {middle + 1}")
-      print(f"The total number of guesses is: {num_guesses}.")
-      return middle, middle+1, num_guesses
+    Return:
+      1. packed tuple containing:
+        * the key's index if found or -1 if the key is not found
+        * value - the value of the key itself
+        * the number of iterations (guesses) until the key was found
 
-  return -1
+    """
+    low = 1
+    high = len(array) - 1
+    num_guesses = 0
+
+    while low <= high:
+        num_guesses += 1
+        middle = (low + high) // 2
+        if array[middle] < key:  # shift left
+            print(f"The number is higher than {middle}.")
+            low = middle + 1
+        elif array[middle] > key:  # shift right
+            print(f"The number is lower than {middle}.")
+            high = middle - 1
+        elif array[middle] == key:
+            print(f"\nThe number is found. It is {middle + 1}")
+            print(f"The total number of guesses is: {num_guesses}.")
+            return middle, middle + 1, num_guesses
+
+    return -1
+
 
 def main():
-  arr = range(1,1000001)
-  guess = []
+    arr = range(1, 1000001)
+    guess = []
 
-  # execute the "game" 100 times and store the number of guesses 
-  for i in range(1,101):
-    r = random.randint(1,1000000)
-    print(f"Guess {i}")
-    index, value, num_guesses = BinarySearch(arr, r)   # index, value go unused
-    guess.append(num_guesses)
-    print()
+    # execute the "game" 100 times and store the number of guesses
+    for i in range(1, 101):
+        r = random.randint(1, 1000000)
+        print(f"Guess {i}")
+        index, value, num_guesses = BinarySearch(arr, r)  # index, value go unused
+        guess.append(num_guesses)
+        print()
 
-  # verify the number of guesses and compute average number over 100 iterations
-  print(f"\nNumber of guesses: {len(guess)}")
-  print(f"Average number of guesses: {sum(guess) / len(guess)}")
+    # verify the number of guesses and compute average number over 100 iterations
+    print(f"\nNumber of guesses: {len(guess)}")
+    print(f"Average number of guesses: {sum(guess) / len(guess)}")
 
-main()    # invoke program
+
+main()  # invoke program
--- Assignments/Programming-Assignment-1/Problem2.py
+++ Assignments/Programming-Assignment-1/Problem2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """
 Name: Devere Anthony Weaver
 
@@ -13,34 +14,37 @@ E.g. If input is 57 then 111001 would be output to stdout.
 
 """
 
+
 def decimalToBinary(dec):
-  """
-  Returns value of given decimal argument to its binary equivalent via a
-  recursive algorithm call.
+    """
+    Returns value of given decimal argument to its binary equivalent via a
+    recursive algorithm call.
 
-  Arguments:
-    1. dec - decimal number to be converted to a binary number
+    Arguments:
+      1. dec - decimal number to be converted to a binary number
 
-  Return: N/A
-  """
-  if ((dec / 2) == 0):
-    pass
-  elif ((dec % 2) == 0):    # even
-    dec = dec // 2
-    decimalToBinary(dec)
-    print("0", end="")
-  elif((dec % 2) != 0):   # odd:
-    dec = dec // 2
-    decimalToBinary(dec)
-    print("1", end="")
+    Return: N/A
+    """
+    if (dec / 2) == 0:
+        pass
+    elif (dec % 2) == 0:  # even
+        dec = dec // 2
+        decimalToBinary(dec)
+        print("0", end="")
+    elif (dec % 2) != 0:  # odd:
+        dec = dec // 2
+        decimalToBinary(dec)
+        print("1", end="")
+
 
 def main():
-  """
-  Main driver
-  """
-  number = int(input("Enter a positive decimal number: "))
-  print(f"The binary number is: ", end="")
-  decimalToBinary(number)
-  print()
+    """
+    Main driver
+    """
+    number = int(input("Enter a positive decimal number: "))
+    print(f"The binary number is: ", end="")
+    decimalToBinary(number)
+    print()
 
-main()    # invoke program
+
+main()  # invoke program
