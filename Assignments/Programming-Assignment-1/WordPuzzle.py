862b372eee688afc8b31fc5cca9e81f3f36d52d8
daw <deveredev@gmail.com>
2023-10-02 12:02:50 -0400

    fixed parsing function

--- Assignments/Programming-Assignment-1/WordPuzzle.py
+++ Assignments/Programming-Assignment-1/WordPuzzle.py
@@ -4,6 +4,9 @@
 TODO: Fill in docstring here
 """
 
+from typing import List
+
+
 def Parse(filename):
     """
     Parse(filename) - parses a given 2D-array character file as input and places
@@ -20,25 +23,18 @@ def Parse(filename):
     cols = 0
     with open("puzzleinput.txt") as f:
         for line in f.readlines():
-            if line.strip():
-                line = line[:-1]   # get rid of newline
-                grid.append(line)
-                cols += len(line)
-                rows += 1
-        cols = int(cols / rows)
-    return grid, rows, cols
-
+            line = line.strip()   # drop whitespace 
+            line = line[:-1]    # drop newline
+            for i in range(len(line)):
+                grid.append(line[i])
     return grid
 
+
 def main():
-    grid, rows, cols = Parse("puzzleinput.txt")
+    grid = Parse("puzzleinput.txt")
     
     # check grid was read correctly
-    for line in grid:
-        print(f"{line}")
-    print(f"Number of rows: {rows}")
-    print(f"Number of columns: {cols}")
-
-            
+    for i in range(len(grid)):
+        print(f"Index {i}: {grid[i]}")
 
 main()
